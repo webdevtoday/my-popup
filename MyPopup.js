2f60d4808a6eb59f24f5b189e0e4426d106aea89
webdevtoday <coderwd@gmail.com>
2020-09-29 07:16:01 +0300

    Start project with files. Create class MyPopup. HTML template for popup.

---
+++ MyPopup.js
@@ -0,0 +1,22 @@
+class MyPopup {
+    template = `
+        <div class="mp-modal">
+            <div class="mp-modal__container">
+                <div class="mp-modal__window">
+                    <div class="mp-modal__header">
+                        <h5 class="mp-modal__title">Title</div>
+                        <button class="mp-modal__close">
+                            <div class="mp-modal__icon-close">&times;</div>
+                        </button>
+                    </div>
+                    <div class="mp-modal__content">
+                        <p>Content</p>
+                    </div>
+                    <div class="mp-modal__footer">
+                        <p>Text/Button/etc.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
